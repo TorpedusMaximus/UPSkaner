f7fabf7f08f01c2d4b5f0d5171a200ab27b708d2
malko <msa>
2022-01-12 15:59:29 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace UPSkaner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skanuj = new System.Windows.Forms.Button();
+            this.drukujSkan = new System.Windows.Forms.PictureBox();
+            this.rozdzielczosc = new System.Windows.Forms.TrackBar();
+            this.etykietaRozdz = new System.Windows.Forms.Label();
+            this.szarosc = new System.Windows.Forms.RadioButton();
+            this.RGB = new System.Windows.Forms.RadioButton();
+            this.bitowy = new System.Windows.Forms.RadioButton();
+            this.trybSkanowania = new System.Windows.Forms.Label();
+            this.zapis = new System.Windows.Forms.ComboBox();
+            this.eksport = new System.Windows.Forms.Label();
+            this.skanery = new System.Windows.Forms.ComboBox();
+            this.etykietaSKan = new System.Windows.Forms.Label();
+            this.wczytajSkanery = new System.Windows.Forms.Button();
+            this.dpi200 = new System.Windows.Forms.Label();
+            this.dpi300 = new System.Windows.Forms.Label();
+            this.wyswietl = new System.Windows.Forms.Label();
+            this.ostrzezenie = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drukujSkan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rozdzielczosc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // skanuj
+            // 
+            this.skanuj.Location = new System.Drawing.Point(42, 356);
+            this.skanuj.Name = "skanuj";
+            this.skanuj.Size = new System.Drawing.Size(348, 48);
+            this.skanuj.TabIndex = 0;
+            this.skanuj.Text = "Skanuj";
+            this.skanuj.UseVisualStyleBackColor = true;
+            this.skanuj.Click += new System.EventHandler(this.skanuj_Click);
+            // 
+            // drukujSkan
+            // 
+            this.drukujSkan.Location = new System.Drawing.Point(424, 12);
+            this.drukujSkan.Name = "drukujSkan";
+            this.drukujSkan.Size = new System.Drawing.Size(364, 426);
+            this.drukujSkan.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.drukujSkan.TabIndex = 1;
+            this.drukujSkan.TabStop = false;
+            // 
+            // rozdzielczosc
+            // 
+            this.rozdzielczosc.LargeChange = 100;
+            this.rozdzielczosc.Location = new System.Drawing.Point(133, 162);
+            this.rozdzielczosc.Maximum = 1200;
+            this.rozdzielczosc.Minimum = 200;
+            this.rozdzielczosc.Name = "rozdzielczosc";
+            this.rozdzielczosc.Size = new System.Drawing.Size(257, 45);
+            this.rozdzielczosc.SmallChange = 100;
+            this.rozdzielczosc.TabIndex = 2;
+            this.rozdzielczosc.TickFrequency = 100;
+            this.rozdzielczosc.Value = 200;
+            this.rozdzielczosc.ValueChanged += new System.EventHandler(this.rozdzielczosc_ValueChanged);
+            // 
+            // etykietaRozdz
+            // 
+            this.etykietaRozdz.AutoSize = true;
+            this.etykietaRozdz.Location = new System.Drawing.Point(39, 162);
+            this.etykietaRozdz.Name = "etykietaRozdz";
+            this.etykietaRozdz.Size = new System.Drawing.Size(78, 13);
+            this.etykietaRozdz.TabIndex = 3;
+            this.etykietaRozdz.Text = "Rozdzielczość:";
+            // 
+            // szarosc
+            // 
+            this.szarosc.AutoSize = true;
+            this.szarosc.Location = new System.Drawing.Point(216, 234);
+            this.szarosc.Name = "szarosc";
+            this.szarosc.Size = new System.Drawing.Size(93, 17);
+            this.szarosc.TabIndex = 4;
+            this.szarosc.TabStop = true;
+            this.szarosc.Text = "Skala szarości";
+            this.szarosc.UseVisualStyleBackColor = true;
+            this.szarosc.CheckedChanged += new System.EventHandler(this.szarosc_CheckedChanged);
+            // 
+            // RGB
+            // 
+            this.RGB.AutoSize = true;
+            this.RGB.Location = new System.Drawing.Point(148, 234);
+            this.RGB.Name = "RGB";
+            this.RGB.Size = new System.Drawing.Size(48, 17);
+            this.RGB.TabIndex = 5;
+            this.RGB.TabStop = true;
+            this.RGB.Text = "RGB";
+            this.RGB.UseVisualStyleBackColor = true;
+            this.RGB.CheckedChanged += new System.EventHandler(this.RGB_CheckedChanged);
+            // 
+            // bitowy
+            // 
+            this.bitowy.AutoSize = true;
+            this.bitowy.Location = new System.Drawing.Point(326, 234);
+            this.bitowy.Name = "bitowy";
+            this.bitowy.Size = new System.Drawing.Size(64, 17);
+            this.bitowy.TabIndex = 6;
+            this.bitowy.TabStop = true;
+            this.bitowy.Text = "1-bitowy";
+            this.bitowy.UseVisualStyleBackColor = true;
+            this.bitowy.CheckedChanged += new System.EventHandler(this.bitowy_CheckedChanged);
+            // 
+            // trybSkanowania
+            // 
+            this.trybSkanowania.AutoSize = true;
+            this.trybSkanowania.Location = new System.Drawing.Point(39, 238);
+            this.trybSkanowania.Name = "trybSkanowania";
+            this.trybSkanowania.Size = new System.Drawing.Size(91, 13);
+            this.trybSkanowania.TabIndex = 7;
+            this.trybSkanowania.Text = "Tryb skanowania:";
+            // 
+            // zapis
+            // 
+            this.zapis.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.zapis.FormattingEnabled = true;
+            this.zapis.Items.AddRange(new object[] {
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".pdf"});
+            this.zapis.Location = new System.Drawing.Point(133, 292);
+            this.zapis.Name = "zapis";
+            this.zapis.Size = new System.Drawing.Size(257, 21);
+            this.zapis.TabIndex = 8;
+            // 
+            // eksport
+            // 
+            this.eksport.AutoSize = true;
+            this.eksport.Location = new System.Drawing.Point(39, 300);
+            this.eksport.Name = "eksport";
+            this.eksport.Size = new System.Drawing.Size(75, 13);
+            this.eksport.TabIndex = 9;
+            this.eksport.Text = "Plik do zapisu:";
+            // 
+            // skanery
+            // 
+            this.skanery.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.skanery.FormattingEnabled = true;
+            this.skanery.Location = new System.Drawing.Point(133, 69);
+            this.skanery.Name = "skanery";
+            this.skanery.Size = new System.Drawing.Size(257, 21);
+            this.skanery.TabIndex = 10;
+            // 
+            // etykietaSKan
+            // 
+            this.etykietaSKan.AutoSize = true;
+            this.etykietaSKan.Location = new System.Drawing.Point(39, 72);
+            this.etykietaSKan.Name = "etykietaSKan";
+            this.etykietaSKan.Size = new System.Drawing.Size(44, 13);
+            this.etykietaSKan.TabIndex = 11;
+            this.etykietaSKan.Text = "Skaner:";
+            // 
+            // wczytajSkanery
+            // 
+            this.wczytajSkanery.Location = new System.Drawing.Point(271, 111);
+            this.wczytajSkanery.Name = "wczytajSkanery";
+            this.wczytajSkanery.Size = new System.Drawing.Size(119, 23);
+            this.wczytajSkanery.TabIndex = 12;
+            this.wczytajSkanery.Text = "Wczytaj skanery";
+            this.wczytajSkanery.UseVisualStyleBackColor = true;
+            this.wczytajSkanery.Click += new System.EventHandler(this.odswierz_Click);
+            // 
+            // dpi200
+            // 
+            this.dpi200.AutoSize = true;
+            this.dpi200.Location = new System.Drawing.Point(133, 193);
+            this.dpi200.Name = "dpi200";
+            this.dpi200.Size = new System.Drawing.Size(46, 13);
+            this.dpi200.TabIndex = 13;
+            this.dpi200.Text = "200 DPI";
+            // 
+            // dpi300
+            // 
+            this.dpi300.AutoSize = true;
+            this.dpi300.Location = new System.Drawing.Point(344, 194);
+            this.dpi300.Name = "dpi300";
+            this.dpi300.Size = new System.Drawing.Size(52, 13);
+            this.dpi300.TabIndex = 14;
+            this.dpi300.Text = "1200 DPI";
+            // 
+            // wyswietl
+            // 
+            this.wyswietl.AutoSize = true;
+            this.wyswietl.Location = new System.Drawing.Point(42, 192);
+            this.wyswietl.Name = "wyswietl";
+            this.wyswietl.Size = new System.Drawing.Size(0, 13);
+            this.wyswietl.TabIndex = 15;
+            // 
+            // ostrzezenie
+            // 
+            this.ostrzezenie.AutoSize = true;
+            this.ostrzezenie.Location = new System.Drawing.Point(39, 316);
+            this.ostrzezenie.Name = "ostrzezenie";
+            this.ostrzezenie.Size = new System.Drawing.Size(229, 13);
+            this.ostrzezenie.TabIndex = 16;
+            this.ostrzezenie.Text = "Wybór .pdf zapisze także kopię w formacie .jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ostrzezenie);
+            this.Controls.Add(this.wyswietl);
+            this.Controls.Add(this.dpi300);
+            this.Controls.Add(this.dpi200);
+            this.Controls.Add(this.wczytajSkanery);
+            this.Controls.Add(this.etykietaSKan);
+            this.Controls.Add(this.skanery);
+            this.Controls.Add(this.eksport);
+            this.Controls.Add(this.zapis);
+            this.Controls.Add(this.trybSkanowania);
+            this.Controls.Add(this.bitowy);
+            this.Controls.Add(this.RGB);
+            this.Controls.Add(this.szarosc);
+            this.Controls.Add(this.etykietaRozdz);
+            this.Controls.Add(this.rozdzielczosc);
+            this.Controls.Add(this.drukujSkan);
+            this.Controls.Add(this.skanuj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.drukujSkan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rozdzielczosc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button skanuj;
+        private System.Windows.Forms.PictureBox drukujSkan;
+        private System.Windows.Forms.TrackBar rozdzielczosc;
+        private System.Windows.Forms.Label etykietaRozdz;
+        private System.Windows.Forms.RadioButton szarosc;
+        private System.Windows.Forms.RadioButton RGB;
+        private System.Windows.Forms.RadioButton bitowy;
+        private System.Windows.Forms.Label trybSkanowania;
+        private System.Windows.Forms.ComboBox zapis;
+        private System.Windows.Forms.Label eksport;
+        private System.Windows.Forms.ComboBox skanery;
+        private System.Windows.Forms.Label etykietaSKan;
+        private System.Windows.Forms.Button wczytajSkanery;
+        private System.Windows.Forms.Label dpi200;
+        private System.Windows.Forms.Label dpi300;
+        private System.Windows.Forms.Label wyswietl;
+        private System.Windows.Forms.Label ostrzezenie;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using WIA;
+using Aspose.Pdf;
+
+namespace UPSkaner
+{
+    public partial class Form1 : Form
+    {
+        static int color_mode = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            RGB.Checked = true;
+            zapis.SelectedIndex = 0;
+            wyswietl.Text = "200 DPI";
+        }
+
+        private void skanuj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String nazwaSkanera = (string)skanery.SelectedItem;
+                DeviceInfo skaner = null;
+                var deviceManager = new DeviceManager();
+
+                for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+                {
+                    if (deviceManager.DeviceInfos[i].Properties["Name"].get_Value() == nazwaSkanera)
+                    {
+                        skaner = deviceManager.DeviceInfos[i];
+                    }
+                }
+
+                if (skaner == null)
+                {
+                    MessageBox.Show("Błędny skaner");
+                    return;
+                }
+
+                var skan = skaner.Connect().Items[1];
+
+                ustawSkaner(skan);
+                ustawKolor(skan);
+                ustawRozdzielczosc(skan);
+
+                ImageFile obraz = skan.Transfer();
+                String sciezka = null;
+                SaveFileDialog okienko = new SaveFileDialog();
+
+                if (okienko.ShowDialog() == DialogResult.OK)
+                {
+                    sciezka = okienko.FileName;
+                    sciezka = sciezka.Split('.')[0];
+                    sciezka += zapis.SelectedItem;
+                    Console.WriteLine(sciezka);
+                }
+                else
+                {
+                    MessageBox.Show("Błędna ścieżka zapisu");
+                    return;
+                }
+
+                if (File.Exists(sciezka))
+                {
+                    File.Delete(sciezka);
+                }
+
+                if (zapis.SelectedItem == ".pdf")
+                {
+                    obraz.SaveFile(sciezka.Split('.')[0] + ".jpg");
+                    String sciezkaPomocnicza = sciezka.Split('.')[0] + ".jpg";
+
+                    Document dokument = new Document();
+                    Page strona = dokument.Pages.Add();
+                    Aspose.Pdf.Image obrazPomocniczy = new Aspose.Pdf.Image();
+                    obrazPomocniczy.File = (sciezkaPomocnicza);
+
+                    System.Drawing.Image wczytanyObraz = System.Drawing.Image.FromFile(sciezkaPomocnicza);
+                    int h = wczytanyObraz.Height;
+                    int w = wczytanyObraz.Width;
+                    strona.PageInfo.Height = (h);
+                    strona.PageInfo.Width = (w);
+                    strona.PageInfo.Margin.Bottom = (0);
+                    strona.PageInfo.Margin.Top = (0);
+                    strona.PageInfo.Margin.Right = (0);
+                    strona.PageInfo.Margin.Left = (0);
+
+                    strona.Paragraphs.Add(obrazPomocniczy);
+                    dokument.Save(sciezka);
+                    drukujSkan.ImageLocation = sciezkaPomocnicza;
+                }
+                else
+                {
+                    obraz.SaveFile(sciezka);
+                    drukujSkan.ImageLocation = sciezka;
+                }
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void odswierz_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var deviceManager = new DeviceManager();
+                for (int i = 1; i <= deviceManager.DeviceInfos.Count; i++)
+                {
+                    if (deviceManager.DeviceInfos[i].Type != WiaDeviceType.ScannerDeviceType)
+                    {
+                        continue;
+                    }
+                    skanery.Items.Add(deviceManager.DeviceInfos[i].Properties["Name"].get_Value());
+                }
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            };
+        }
+
+
+        ////////////////////////USTAWIENIE SKANERA////////////////////////////////
+
+        private static void ustawSkaner(IItem skan)
+        {
+            const string WIA_HORIZONTAL_SCAN_SIZE_PIXELS = "6151";
+            const string WIA_VERTICAL_SCAN_SIZE_PIXELS = "6152";
+
+            zmienOpcje(skan.Properties, WIA_HORIZONTAL_SCAN_SIZE_PIXELS, 2500);
+            zmienOpcje(skan.Properties, WIA_VERTICAL_SCAN_SIZE_PIXELS, 3500);
+        }
+
+        private void ustawRozdzielczosc(IItem skan)
+        {
+            const string WIA_HORIZONTAL_SCAN_RESOLUTION_DPI = "6147";
+            const string WIA_VERTICAL_SCAN_RESOLUTION_DPI = "6148";
+
+            zmienOpcje(skan.Properties, WIA_HORIZONTAL_SCAN_RESOLUTION_DPI, rozdzielczosc.Value);
+            zmienOpcje(skan.Properties, WIA_VERTICAL_SCAN_RESOLUTION_DPI, rozdzielczosc.Value);
+        }
+
+        private static void ustawKolor(IItem skan)
+        {
+            const string WIA_SCAN_COLOR_MODE = "6146";
+            zmienOpcje(skan.Properties, WIA_SCAN_COLOR_MODE, color_mode);
+        }
+
+        private static void zmienOpcje(IProperties properties, object propName, object propValue)
+        {
+            Property prop = properties.get_Item(ref propName);
+            prop.set_Value(ref propValue);
+        }
+
+
+        ////////////////////////USTAWIENIE KOLORU////////////////////////////////
+
+        private void szarosc_CheckedChanged(object sender, EventArgs e)
+        {
+            if (szarosc.Checked == true)
+            {
+                color_mode = 2;
+            }
+        }
+
+        private void RGB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (RGB.Checked == true)
+            {
+                color_mode = 1;
+            }
+        }
+
+        private void bitowy_CheckedChanged(object sender, EventArgs e)
+        {
+            if (bitowy.Checked == true)
+            {
+                color_mode = 4;
+            }
+        }
+
+
+        private void rozdzielczosc_ValueChanged(object sender, EventArgs e)
+        {
+            if (rozdzielczosc.Value % 100 < 50)
+            {
+                rozdzielczosc.Value -= rozdzielczosc.Value % 100;
+            }
+            else
+            {
+                rozdzielczosc.Value -= rozdzielczosc.Value % 100;
+                rozdzielczosc.Value += 100;
+            }
+            wyswietl.Text = rozdzielczosc.Value.ToString() + " DPI";
+        }
+    }
+}
